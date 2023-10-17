4c0c22b5b72ba8d4cc06ae9889e35173cc0f30c8
Reactongarph <js.developer@ongraph.com>
2023-10-17 18:55:18 +0530

    fetch data from api

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,20 @@
 import LoginPage from "./component/loginPage";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch(
+    "https://llw-backend.vercel.app/v1/master/article-category/list"
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <main>
       <LoginPage />
